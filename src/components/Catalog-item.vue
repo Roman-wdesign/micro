416429e278a1e923545bed568f7155447e39d358
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-07-11 10:41:35 +0700

    2

--- src/components/Catalog-item.vue
+++ src/components/Catalog-item.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="item__catalog">
-    <div class="item__catalog-card"></div>
+    <div class="item__catalog-card">
+      <p>{{ animal_data.category }}</p>
+      <p>1</p>
+      <div class="item__catalog-img"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Catalog-item",
+
+  data: () => ({}),
+
   props: {
     animal_data: {
       type: Object,
--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="catalog"></div>
+  <div class="catalog">
+    <div class="catalog__container_currency">
+      <CatalogItem
+        v-for="(animal, id) in ANIMALS"
+        :key="id"
+        :category="animal.category"
+        :timestamp="animal.timestamp"
+        :filesize="animal.filesize"
+        :image="animal.image"
+        :animal_data="animal"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import CatalogItem from "./Catalog-item";
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "Catalog",
+  components: { CatalogItem },
+  data: () => ({}),
+
+  computed: {
+    ...mapGetters(["ANIMALS"]),
+  },
+  methods: {
+    ...mapActions(["GET_ANIMALS_FROM_API"]),
+  },
+  mounted() {
+    this.GET_ANIMALS_FROM_API();
+  },
 };
 </script>
 
