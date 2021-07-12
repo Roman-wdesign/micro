317fc6374200b227e2f186d4cc02bfe8cf03c5fb
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-07-12 09:20:02 +0700

    paginate

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -2,7 +2,7 @@
   <div class="catalog">
     <div class="catalog__container_currency">
       <CatalogItem
-        v-for="(animal, id) in ANIMALS"
+        v-for="(animal, id) in paginatedAnimals"
         :key="id"
         :category="animal.category"
         :timestamp="animal.timestamp"
@@ -10,6 +10,17 @@
         :image="animal.image"
         :animal_data="animal"
       />
+      <div class="catalog__pagination">
+        <button
+          class="catalog__pagination__page"
+          v-for="page in pages"
+          :key="page"
+          :class="{ catalog__pagination__page__selected: page === pageFirst }"
+          @click="pageChangeClick(page)"
+        >
+          {{ page }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -21,18 +32,34 @@ import { mapActions, mapGetters } from "vuex";
 export default {
   name: "Catalog",
   components: { CatalogItem },
-  data: () => ({}),
+  data: () => ({
+    animalsOnPage: 50,
+    pageFirst: 1,
+  }),
 
   computed: {
     ...mapGetters(["ANIMALS"]),
+    pages() {
+      return Math.ceil(this.ANIMALS.length / 10);
+    },
+    paginatedAnimals() {
+      let from = (this.pageFirst - 1) * this.animalsOnPage;
+      let to = from + this.animalsOnPage;
+      return this.ANIMALS.slice(from, to);
+    },
   },
   methods: {
     ...mapActions(["GET_ANIMALS_FROM_API"]),
+    pageChangeClick(page) {
+      this.pageFirst = page;
+    },
   },
   mounted() {
     this.GET_ANIMALS_FROM_API();
   },
+  pageChangeClick(page) {
+    this.pageFirst = page;
+  },
 };
 </script>
-
-<style scoped></style>
+<style></style>
