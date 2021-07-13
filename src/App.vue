1e750728d3fd7a658765e63ef782590dcdac736d
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-07-13 09:13:03 +0700

    mixins.scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <Header />
+    <div class="radio">
+      <input type="radio" value="cards" view v-model="selected" />cards view<br />
+      <input type="radio" value="tree" view v-model="selected" />tree view<br />
+      <p>Выбрано: {{ selected }}</p>
+    </div>
     <Catalog />
     <Footer />
   </div>
@@ -11,7 +16,11 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Catalog from "./components/Catalog";
+
 export default {
   components: { Catalog, Footer, Header },
+  data: () => ({
+    selected: "cards",
+  }),
 };
 </script>
